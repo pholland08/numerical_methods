6d28adc4832123f8a97d806cf8cbba4256762c08
Phillip Holland <phillip.holland328@gmail.com>
2019-09-09 22:22:34 -0500

    Added support for iteration count and max to bisection method.

--- cookie_cutters/methods.py
+++ cookie_cutters/methods.py
@@ -1,38 +1,70 @@
+import math
 
 
-def bisect(fn: callable, bounds: (float, float), tolerance: float) -> float:
+def bisect(fn: callable,
+           bounds: (float, float),
+           tolerance: float,
+           current_iteration: float = 0,
+           max_iterations: float = 0) -> dict:
     """
     :param fn:
     :param bounds: Tuple for lower bound, upper bound
     :param tolerance:
-    :return: float
+    :param current_iteration:
+    :param max_iterations:
+    :return: {root: float, iterations: int, max_iterations: int}
     """
+
+    # todo: calculate precision -> ((new - old)/new)*100
+
     lower_bound = min(bounds) if min(bounds) != 0 else tolerance/2
     upper_bound = max(bounds) if max(bounds) != 0 else -tolerance/2
 
     lower_result = float(fn(lower_bound))
     upper_result = float(fn(upper_bound))
+    current_iteration = current_iteration + 1
+
+    if max_iterations == 0:
+        max_iterations = calculate_bisection_iterations(bounds, tolerance)
 
     if abs(lower_result) < abs(tolerance):
-        return lower_bound
+        return {"root": lower_bound,
+                "iterations": current_iteration,
+                "max_iterations": max_iterations}
 
     elif abs(upper_result) < abs(tolerance):
-        return upper_bound
+        return {"root": upper_bound,
+                "iterations": current_iteration,
+                "max_iterations": max_iterations}
 
     elif lower_result * upper_result < 0:
         mid_point = (lower_bound + upper_bound)/2
-        mid_result = float(fn(mid_point))
+        mid_result = fn(mid_point)
 
         if abs(mid_result) < abs(tolerance):
-            return mid_point
+            return {"root": mid_point,
+                    "iterations": current_iteration,
+                    "max_iterations": max_iterations}
 
-        elif mid_result * lower_result < 0:
-            return bisect(fn, (lower_bound, mid_point), tolerance)
+        elif signs_differ(mid_result, lower_result):
+            return bisect(fn, (lower_bound, mid_point), tolerance, current_iteration, max_iterations)
 
-        elif mid_result * upper_result < 0:
-            return bisect(fn, (mid_point, upper_bound), tolerance)
+        elif signs_differ(mid_result, upper_result):
+            return bisect(fn, (mid_point, upper_bound), tolerance, current_iteration, max_iterations)
 
     raise Exception("Root not found")
+
+
+def signs_differ(n1: float, n2: float):
+    return n1 * n2 < 0
+
+
+def calculate_bisection_iterations(bounds: tuple, tolerance: float) -> int:
+    return math.ceil(math.log(abs(max(bounds)-min(bounds))/tolerance, 2))
+
+
+def calculate_bisection_precision(current_value: float, previous_value: float) -> float:
+    return abs((current_value - previous_value)/current_value) * 100
 
 
 def newton_raphson(fn: callable, dfn: callable, x_initial: float, max_iterations: int) -> float:
@@ -43,6 +75,10 @@ def newton_raphson(fn: callable, dfn: callable, x_initial: float, max_iterations
     :param max_iterations:
     :return:
     """
+
+    # todo: calculate precision
+    # todo: keep list of previous values to check for oscillation
+
     if max_iterations > 0:
         fx = x_initial - (fn(x_initial) / dfn(x_initial))
         return newton_raphson(fn, dfn, fx, max_iterations-1)
@@ -51,6 +87,5 @@ def newton_raphson(fn: callable, dfn: callable, x_initial: float, max_iterations
         return x_initial
 
 
-if __name__ == '__main__':
-    print(bisect(lambda x: x**3, (-1, 1), .00001))
-    print(newton_raphson(lambda x: x**3 - 20, lambda x: 3*(x**2), 3, 3))
+def secant(fn: callable, x: float) -> float:
+    pass
--- test_cookie_cutters/test_methods/TestBisection.py
+++ test_cookie_cutters/test_methods/TestBisection.py
@@ -2,24 +2,42 @@ from cookie_cutters import methods
 from unittest import TestCase
 import math
 
-tolerance = 10 ** -3
+tolerance = 10 ** -8
 
 
 class TestBisection(TestCase):
+
+    def test_calculate_bisection_precision(self):
+        precision = methods.calculate_bisection_precision(3.25, 2.5)
+        self.assertAlmostEqual(23.0769, precision, 4)
+
+    def test_calculate_bisection_iterations(self):
+        result = methods.calculate_bisection_iterations((0, 1), .001)
+        self.assertEqual(10, result)
 
     def test_returns_within_bounds(self):
         lower_bound = -1
         upper_bound = 1
         result = methods.bisect(lambda x: (x**3), (lower_bound, upper_bound), tolerance)
-        self.assertTrue(lower_bound < result < upper_bound)
+        self.assertTrue(lower_bound < result["root"] < upper_bound)
 
     def test_returns_within_tolerance(self):
         result = methods.bisect(lambda x: (x**3 - 20), (-1, 4), tolerance)
-        self.assertTrue(2.7144 - tolerance < result < 2.7144 + tolerance)
+        self.assertTrue(2.71441762 - tolerance < result["root"] < 2.71441762 + tolerance)
 
     def test_midpoint_root_succeeds(self):
         result = methods.bisect(lambda x: x**3, (-1, 1), tolerance)
-        self.assertTrue(-tolerance < result < tolerance)
+        self.assertTrue(-tolerance < result["root"] < tolerance)
+
+    def test_expected_iterations(self):
+        result = methods.bisect(lambda x: x**3 - 20, (0, 5), tolerance)
+        self.assertGreaterEqual(result["max_iterations"], result["iterations"])
+
+    def test_max_iterations(self):
+        fn = lambda x: x**3 - 20
+        bounds = (0, 5)
+        result = methods.bisect(fn, bounds, tolerance)
+        self.assertGreaterEqual(result["max_iterations"], methods.calculate_bisection_iterations(bounds, tolerance))
 
     def test_fn_a(self):
         """
@@ -27,7 +45,7 @@ class TestBisection(TestCase):
         """
 
         result = methods.bisect(lambda x: (1/x) - 5, (0, .25), tolerance)
-        self.assertTrue(.2 - tolerance < result < .2 + tolerance)
+        self.assertTrue(.2 - tolerance <= result["root"] <= .2 + tolerance)
 
     def test_fn_b(self):
         """
@@ -35,7 +53,7 @@ class TestBisection(TestCase):
         """
 
         result = methods.bisect(lambda x: math.e**x - 2, (0, 1), tolerance)
-        self.assertTrue(.69315 - tolerance < result < .69315 + tolerance)
+        self.assertTrue(0.69314718 - tolerance <= result["root"] <= 0.69314718 + tolerance)
 
     def test_fn_c(self):
         """
@@ -43,23 +61,23 @@ class TestBisection(TestCase):
         """
 
         result = methods.bisect(lambda x: x**3 - 2, (0, 2), tolerance)
-        self.assertTrue(1.2599 - tolerance < result < 1.2599 + tolerance)
+        self.assertTrue(1.259921049 - tolerance <= result["root"] <= 1.259921049 + tolerance)
 
     def test_fn_d(self):
         """
         x^2=sin(x), x_0=1
         """
 
-        result = methods.bisect(lambda x: math.sin(x) - (x**2), (0, 1), tolerance)
-        self.assertTrue(.8767125 - tolerance < result < .8767125 + tolerance)
+        result = methods.bisect(lambda x: math.sin(x) - (x**2), (.25, 1), tolerance)
+        self.assertTrue(0.876726215 - tolerance <= result["root"] <= 0.876726215 + tolerance)
 
     def test_fn_e(self):
         """
         x=tan(x), x_0=4.5
         """
 
-        result = methods.bisect(lambda x: math.tan(x) - x, (1, 4.5), tolerance)
-        self.assertTrue(4.4935 - tolerance < result < 4.4935 + tolerance)
+        result = methods.bisect(lambda x: math.tan(x) - x, (2, 4.5), tolerance)
+        self.assertTrue(4.49340946 - tolerance <= result["root"] <= 4.49340946 + tolerance)
 
     def test_fn_f(self):
         """
